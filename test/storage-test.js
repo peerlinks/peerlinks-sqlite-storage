d22dfc7df4439cf06a2bbb30a7700ce6af242622
Fedor Indutny <fedor@indutny.com>
2019-08-14 23:48:44 -0700

    storage: finish

--- test/storage-test.js
+++ test/storage-test.js
@@ -190,8 +190,8 @@ describe('NodeStorage', () => {
 
     assert.deepStrictEqual(await storage.getEntityKeys('fake'), [ 'id' ]);
 
-    const retrieve = await storage.retrieveEntity('fake', 'id', Fake);
-    assert.strictEqual(retrieve.text, 'hello');
+    const blob = await storage.retrieveEntity('fake', 'id', Fake);
+    assert.strictEqual(Fake.deserializeData(blob).text, 'hello');
 
     const missing = await storage.retrieveEntity('fake', randomBytes(32), Fake);
     assert.ok(!missing);
