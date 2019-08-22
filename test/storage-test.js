a0187fa0eedc02705786e2aaaff11fcd61083b12
Fedor Indutny <fedor@indutny.com>
2019-08-22 12:45:41 -0700

    storage: fix `getMessage` on huge hash count

--- test/storage-test.js
+++ test/storage-test.js
@@ -5,7 +5,9 @@ import { randomBytes } from 'crypto';
 
 import Storage from '../';
 
-describe('Storage', () => {
+describe('Storage', function() {
+  this.timeout(20000);
+
   let channelId = null;
   let storage = null;
 
@@ -241,5 +243,24 @@ describe('Storage', () => {
       '0: a',
       '1: c',
     ]);
+  });
+
+  it('should load tons of messages with getMessages', async () => {
+    const hashes = [];
+    for (let i = 0; i < 3000; i++ ){
+      let padded = i.toString();
+      while (padded.length < 4) {
+        padded = '0' + padded;
+      }
+      const hash = `h${padded}`;
+      hashes.push(Buffer.from(hash));
+      await storage.addMessage(msg(hash, 0));
+    }
+
+    const messages = await storage.getMessages(channelId, hashes);
+    assert.strictEqual(messages.length, hashes.length);
+    for (let i = 0; i < messages.length; i++) {
+      assert.strictEqual(messages[i].toString(), '0: ' + hashes[i]);
+    }
   });
 });
